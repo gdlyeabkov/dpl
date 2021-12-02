369f11230527178c839b1bdb54df307f98c9e70f
gdlyeabkov <gdlyeabkov@gmail.com>
2021-12-02 11:00:45 +0300

    addviews

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Playground from '../views/Playground.vue'
+import Blog from '../views/Blog.vue'
+import WhyLang from '../views/WhyLang.vue'
+import Docs from '../views/Docs.vue'
+import GetStarted from '../views/GetStarted.vue'
+import Packages from '../views/Packages.vue'
+
 
 Vue.use(VueRouter)
 
@@ -9,6 +16,36 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/playground',
+    name: 'Playground',
+    component: Playground
+  },
+  {
+    path: '/blog',
+    name: 'Blog',
+    component: Blog
+  },
+  {
+    path: '/docs',
+    name: 'Docs',
+    component: Docs
+  },
+  {
+    path: '/getstarted',
+    name: 'GetStarted',
+    component: GetStarted
+  },
+  {
+    path: '/packages',
+    name: 'Packages',
+    component: Packages
+  },
+  {
+    path: '/why',
+    name: 'WhyLang',
+    component: WhyLang
   }
 ]
 
---
+++ src/views/WhyLang.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <Header />
+        <Footer />
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
